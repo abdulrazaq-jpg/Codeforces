b42621862e275b78ca99acdec18678d1ee1902a5
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:24:17 +0530

    Add El fucho [A] from Codeforces

---
+++ 2155/A - El fucho/solution.cpp
@@ -0,0 +1,30 @@
+// El fucho
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    cout << 2*(n-1) << "
+";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
