983992de06d9914caf9a5a4e0430ccf8201a8ce2
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:14:18 +0530

    Add Convergence [A] from Codeforces

---
+++ 2232/A - Convergence/solution.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+ 
+        vector<int> v;
+ 
+        while (n--)
+        {
+            int x;
+            cin >> x;
+ 
+            v.push_back(x);
+        }
+ 
+        sort(v.begin(), v.end(), less<int>());
+ 
+        int cnt = 0;
+        int size = v.size();
+        int mid = v[size / 2];
+ 
+        for (int i = 0; i < size / 2; i++)
+        {
+            if (v[i] != mid || v[size-i-1] != mid)
+            {
+                v[i] = mid;
+                cnt++;
+            }
+            else
+                i++;
+        }
+ 
+        cout << cnt << "
+";
+    }
+ 
+    return 0;
+}
