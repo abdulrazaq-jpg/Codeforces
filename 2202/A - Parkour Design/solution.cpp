3ae2605742c30ceb6380d609cafa69054c325f70
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:17:45 +0530

    Add Parkour Design [A] from Codeforces

---
+++ 2202/A - Parkour Design/solution.cpp
@@ -0,0 +1,58 @@
+// Parkour Design
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int x,y;
+    cin >> x >> y;
+ 
+    bool flag1 = false;
+ 
+    if((x-(2*y)) % 3 == 0)
+    flag1 =true;
+ 
+    bool flag2 = false;
+ 
+    if((y>=0) && (x>=2*y))
+    {
+        flag2 = true;
+    }
+ 
+    if ((y < 0) && (x >= (-4*y)))
+    {
+        flag2 = true;
+    }
+    
+ 
+    if(flag1 && flag2)
+    {
+        cout << "yes" << "
+";
+    }
+    else
+    {
+        cout << "no" << "
+";
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
+ 
+// SAW THE HINT
