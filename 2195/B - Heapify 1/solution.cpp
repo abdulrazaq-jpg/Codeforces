11c3f388b49e5ec6690a69db7e56ff5e61e62d95
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:21:11 +0530

    Add Heapify 1 [B] from Codeforces

---
+++ 2195/B - Heapify 1/solution.cpp
@@ -0,0 +1,69 @@
+// Heapify 1
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n; cin >> n;
+ 
+    vector<int> v(n+1);
+ 
+    for(int i = 1; i <= n ; i++)
+    {
+        cin >> v[i];
+    }
+ 
+    for(int  i = 1; i <= n ; i+=2)
+    {
+        for(int j = i; j <= n; j*=2)
+        {
+            for(int k = j; k<=n; k*=2)
+            {
+                if(v[k] < v[j])
+                {
+                    swap(v[j],v[k]);
+                }
+            }
+        }
+    }
+ 
+    bool flag = true;
+ 
+    for(int i = 1; i < n; i++)
+    {
+        if(v[i] > v[i+1])
+        {
+            flag = false;
+            break;
+        }
+    }
+ 
+    if(flag)
+    {
+        cout << "YES
+";
+    }
+    else
+    {
+        cout << "NO
+";
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
