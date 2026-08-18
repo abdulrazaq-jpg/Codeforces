1312ba4950d94e4445a1ae461d46b762edb9d808
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:19:29 +0530

    Add Nikita and Books [B] from Codeforces

---
+++ 2244/B - Nikita and Books/solution.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    vector<long long> a(n);
+ 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+ 
+    bool flag = true;
+ 
+    long long store = 0;
+    int i = 0;
+    int track = n;
+ 
+    while(track--)
+    {
+        a[i] += store;
+        store = 0;
+        long long x = a[i] - (i + 1);
+ 
+        if(x <= 0)
+        {
+            i++;
+        }
+ 
+        else
+        {
+            a[i] = (i+1);
+            store = x;
+            i++;
+        }
+    }
+ 
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i - 1] >= a[i])
+        {
+            flag = false;
+            break;
+        }
+    }
+ 
+    if (flag)
+    {
+        cout << "YES
+";
+    }
+ 
+    else
+    {
+        cout << "NO
+";
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
