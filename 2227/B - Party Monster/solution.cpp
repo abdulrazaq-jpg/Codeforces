914d2b90786c718830a0a3b3a394cb18a8452ca2
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:15:44 +0530

    Add Party Monster [B] from Codeforces

---
+++ 2227/B - Party Monster/solution.cpp
@@ -0,0 +1,48 @@
+// Party Monster
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    string s;
+    cin >> s;
+ 
+    int cnt1 = 0, cnt2 = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '(')
+            cnt1++;
+ 
+        else
+            cnt2++;
+    }
+ 
+    if (cnt1 == cnt2)
+        cout << "YES" << "
+";
+    else
+        cout << "NO" << "
+";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
