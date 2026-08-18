5972dc8c1193ef705e95e9d8825c099ff1ffc4ca
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:22:12 +0530

    Add Maximum Neighborhood [A] from Codeforces

---
+++ 2170/A - Maximum Neighborhood/solution.cpp
@@ -0,0 +1,77 @@
+// Maximum Neighbourhood
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    int arr[101][101];
+ 
+    int x = 1;
+ 
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            arr[i][j] = x;
+            x++;
+        }
+    }
+ 
+    if(n==1)
+    {
+        cout << 1 << "
+";
+        return;
+    }
+ 
+    if(n==2)
+    {
+        cout << 9 << "
+";
+        return;
+    }
+ 
+    if(n==3)
+    {
+        cout << 29 << "
+";
+        return;
+    }
+ 
+    if(n==4)
+    {
+        cout << 56 << "
+";
+        return;
+    }
+ 
+    if(n > 4)
+    {
+        int ans = arr[n-1][n-2] + arr[n-1][n-1] + arr[n-1][n] + arr[n-2][n-1] + arr[n][n-1];
+ 
+        cout << ans << "
+";
+        return;
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
