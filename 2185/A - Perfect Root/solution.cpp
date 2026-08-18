319170e4c86ce17f426f3d86133ae21147e27813
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:19:11 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,35 @@
+// Perfect Root
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+ 
+    cout << "
+";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
