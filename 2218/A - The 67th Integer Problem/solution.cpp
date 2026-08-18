51b5e52ff05a4a9e9b5475f47df6ad274292d04e
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:16:25 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.cpp
@@ -0,0 +1,35 @@
+// The 67th Integer Problem
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int x;
+    cin >> x;
+ 
+    if (x >= 66)
+        cout << 67 << "
+";
+ 
+    else
+        cout << x + 2 << "
+";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
