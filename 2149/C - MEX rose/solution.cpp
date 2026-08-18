73fc8437f7db5ce8fede5965c27b7ca58584e197
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:25:36 +0530

    Add MEX rose [C] from Codeforces

---
+++ 2149/C - MEX rose/solution.cpp
@@ -0,0 +1,63 @@
+// MEX rose
+ 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    int k;
+    cin >> k;
+ 
+    int cnt_k = 0;
+ 
+    vector<int> missing(n+1,0);
+ 
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+ 
+        if (x == k)
+        {
+            cnt_k++;
+        }
+ 
+        if(x <= n)
+        {
+            missing[x]++;
+        }
+    }
+ 
+    int cnt = 0;
+    for(int i = 0; i < k; i++)
+    {
+        if(missing[i] == 0)
+        {
+            cnt++;
+        }
+    }
+    cout << max(cnt , cnt_k) << "
+";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
