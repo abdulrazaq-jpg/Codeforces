8566847df85f010507a067657c044d0fb9e356cd
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:19:35 +0530

    Add Crimson Triples [B] from Codeforces

---
+++ 2238/B - Crimson Triples/solution.cpp
@@ -0,0 +1,37 @@
+// Crimsom Triples
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    long long n;
+    cin >> n;
+ 
+    long long ans = 0;
+ 
+    for(int b = 1; b <= n ; b++)
+    {
+        ans += 1ll * (n/b) * (n/b);
+    }
+ 
+    cout << ans << "
+";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
