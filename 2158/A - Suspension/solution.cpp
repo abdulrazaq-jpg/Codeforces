5f7e6128ee7c2a1d415a97b2f0e0d64b0c548782
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:24:03 +0530

    Add Suspension [A] from Codeforces

---
+++ 2158/A - Suspension/solution.cpp
@@ -0,0 +1,41 @@
+// Suspension
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+void solve()
+{
+    int n; cin >> n;
+    int y , r;
+    cin >> y >> r;
+ 
+    int p = r + (y/2);
+ 
+    if(p > n)
+    {
+        cout << n << "
+";
+    }
+    else
+    {
+        cout << p << "
+";
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
