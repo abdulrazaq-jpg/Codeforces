14d2b0175fc0a8ea55cc7469e679017a2833ec77
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-18 13:21:55 +0530

    Add ASCII Art Contest [A] from Codeforces

---
+++ 2172/A - ASCII Art Contest/solution.cpp
@@ -0,0 +1,54 @@
+// ASCII Art Contest
+ 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+void solve()
+{
+    int g,c,l;
+    cin >> g >> c >> l;
+ 
+    vector<int> v;
+ 
+    v.push_back(g);
+    v.push_back(c);
+    v.push_back(l);
+ 
+    sort(v.begin(),v.end());
+ 
+    int M = v[2];
+    int m = v[0];
+ 
+    //cout << M << " " << m << "
+";
+ 
+    int d1 = abs(g-c);
+    int d2 = abs(c-l);
+    int d3 = abs(g-l);
+ 
+    //cout << d1 << " " << d2 << " " << d3 << "
+";
+ 
+    if(d1>=10 || d2>=10 || d3>=10)
+    {
+        cout << "check again
+";
+    }
+    else
+    {
+        cout << "final " << v[1] <<"
+";
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+ 
+    return 0;
+}
